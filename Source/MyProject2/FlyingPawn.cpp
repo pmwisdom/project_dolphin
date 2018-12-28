bb6c7a5423d07beb8298093e5cabbc79aab522e5
William Burdett <william.burdett1@gmail.com>
2018-12-27 23:59:34 -0500

    Movement!

---
+++ Source/MyProject2/FlyingPawn.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FlyingPawn.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Engine/World.h"
+#include "Engine/StaticMesh.h"
+#include "Engine/GameEngine.h"
+
+// Sets default values
+AFlyingPawn::AFlyingPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Structure to hold one-time initialization
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinderOptional<UStaticMesh> PlaneMesh;
+		FConstructorStatics()
+			: PlaneMesh(TEXT("/Game/Flying/Meshes/UFO.UFO"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics;
+
+	RootMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cube Mesh"));
+	RootMesh->SetVisibility(false);
+	RootComponent = RootMesh;
+
+	// Create static mesh component
+	PlaneMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlaneMesh0"));
+	PlaneMesh->SetStaticMesh(ConstructorStatics.PlaneMesh.Get());	// Set static mesh
+	PlaneMesh->SetupAttachment(RootComponent);
+
+	// Create a spring arm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm0"));
+	SpringArm->SetupAttachment(RootComponent);	// Attach SpringArm to RootComponent
+	SpringArm->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
+	SpringArm->SocketOffset = FVector(0.f, 0.f, 60.f);
+	SpringArm->bEnableCameraLag = true;	// Do not allow camera to lag
+	SpringArm->CameraLagSpeed = 10.f;
+	SpringArm->bEnableCameraRotationLag = true;	// Do not allow camera to lag
+	SpringArm->CameraRotationLagSpeed = 10.f;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = true;
+
+
+	// Create camera component 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera0"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);	// Attach the camera
+	Camera->bUsePawnControlRotation = false; // Don't rotate camera with controller
+
+	TurnSpeed = 100.f;
+}
+
+// Called when the game starts or when spawned
+void AFlyingPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFlyingPawn::Tick(float DeltaTime)
+{
+	const FVector LocalMove = FVector(1000.f * DeltaTime, 0.f, 0.f);
+	AddActorLocalOffset(LocalMove, true);
+
+	FRotator CurrentRotation = GetActorRotation();
+	CurrentRotation.Roll = 0;
+	SetActorRotation(CurrentRotation);
+}
+
+// Called to bind functionality to input
+void AFlyingPawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// Check if PlayerInputComponent is valid (not NULL)
+	check(PlayerInputComponent);
+
+	// Bind our control axis' to callback functions
+	PlayerInputComponent->BindAxis("MoveUp", this, &AFlyingPawn::VerticalInput);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFlyingPawn::HoriontalInput);
+}
+
+void AFlyingPawn::VerticalInput(float Input) {
+	// Calculate change in rotation this frame
+	FRotator DeltaRotation(-1 * Input * TurnSpeed * GetWorld()->GetDeltaSeconds(), 0, 0);
+
+	// Rotate plane
+	AddActorLocalRotation(DeltaRotation);
+
+}
+
+void AFlyingPawn::HoriontalInput(float Input) {
+	// Calculate change in rotation this frame
+	FRotator DeltaRotation(0, Input * TurnSpeed * GetWorld()->GetDeltaSeconds(), 0);
+
+	// Rotate plane
+	AddActorLocalRotation(DeltaRotation);
+}
---
+++ Source/MyProject2/FlyingPawn.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "FlyingPawn.generated.h"
+
+UCLASS()
+class MYPROJECT2_API AFlyingPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AFlyingPawn();
+
+	/** StaticMesh component that will be the visuals for our flying pawn */
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* PlaneMesh;
+
+	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* RootMesh;
+
+
+	/** Spring arm that will offset the camera */
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* SpringArm;
+
+	/** Camera component that will be our viewpoint */
+	UPROPERTY(Category = Camera, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* Camera;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void VerticalInput(float input);
+
+	void HoriontalInput(float input);
+
+private:
+	float TurnSpeed;
+};
